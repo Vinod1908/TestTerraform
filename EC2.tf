2e70436144c221a73dccdb341c2d9add5e7d94de
Vinod1908 <55542031+Vinod1908@users.noreply.github.com>
2020-04-16 15:03:45 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -18,29 +18,29 @@ resource "aws_cloudwatch_event_rule" "stop_instances_event_rule" {
 # Event target: Associates a rule with a function to run
 resource "aws_cloudwatch_event_target" "start_instances_event_target" {
   target_id = "start_instances_lambda_target"
-  rule = "${aws_cloudwatch_event_rule.start_instances_event_rule.name}"
-  arn = "${aws_lambda_function.ec2_start_scheduler_lambda.arn}"
+  rule = {aws_cloudwatch_event_rule.start_instances_event_rule.name}
+  arn = {aws_lambda_function.ec2_start_scheduler_lambda.arn}
 }
 
 resource "aws_cloudwatch_event_target" "stop_instances_event_target" {
   target_id = "stop_instances_lambda_target"
-  rule = "${aws_cloudwatch_event_rule.stop_instances_event_rule.name}"
-  arn = "${aws_lambda_function.ec2_stop_scheduler_lambda.arn}"
+  rule = {aws_cloudwatch_event_rule.stop_instances_event_rule.name}
+  arn = {aws_lambda_function.ec2_stop_scheduler_lambda.arn}
 }
 
 # AWS Lambda Permissions: Allow CloudWatch to execute the Lambda Functions
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_start_scheduler" {
   statement_id = "AllowExecutionFromCloudWatch"
   action = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.ec2_start_scheduler_lambda.function_name}"
+  function_name = {aws_lambda_function.ec2_start_scheduler_lambda.function_name}
   principal = "events.amazonaws.com"
-  source_arn = "${aws_cloudwatch_event_rule.start_instances_event_rule.arn}"
+  source_arn = {aws_cloudwatch_event_rule.start_instances_event_rule.arn}
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_stop_scheduler" {
   statement_id = "AllowExecutionFromCloudWatch"
   action = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.ec2_stop_scheduler_lambda.function_name}"
+  function_name = {aws_lambda_function.ec2_stop_scheduler_lambda.function_name}
   principal = "events.amazonaws.com"
-  source_arn = "${aws_cloudwatch_event_rule.stop_instances_event_rule.arn}"
+  source_arn = {aws_cloudwatch_event_rule.stop_instances_event_rule.arn}
 }
