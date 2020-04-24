5e0fd49ade2986b5af35d843f7f3aed10e7904db
Vinod1908 <55542031+Vinod1908@users.noreply.github.com>
2020-04-24 17:43:57 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,6 +1,46 @@
-resource "aws_cloudformation_stack" "instance-scheduler" {
-  name = "instance-scheduler"
+### Cloudwatch Events ###
+# Event rule: Runs at 8pm during working days
+resource "aws_cloudwatch_event_rule" "start_instances_event_rule" {
+  name = "start_instances_event_rule"
+  description = "Starts stopped EC2 instances"
+  schedule_expression = "cron(0 8 ? * MON-FRI *)"
+  depends_on = ["aws_lambda_function.ec2_start_scheduler_lambda"]
+}
 
-  template_url = "https://s3.amazonaws.com/solutions-reference/aws-instance-scheduler/latest/instance-scheduler.template"
+# Runs at 8am during working days
+resource "aws_cloudwatch_event_rule" "stop_instances_event_rule" {
+  name = "stop_instances_event_rule"
+  description = "Stops running EC2 instances"
+  schedule_expression = "cron(0 20 ? * MON-FRI *)"
+  depends_on = ["aws_lambda_function.ec2_stop_scheduler_lambda"]
+}
 
+# Event target: Associates a rule with a function to run
+resource "aws_cloudwatch_event_target" "start_instances_event_target" {
+  target_id = "start_instances_lambda_target"
+  rule = "${aws_cloudwatch_event_rule.start_instances_event_rule.name}"
+  arn = "${aws_lambda_function.ec2_start_scheduler_lambda.arn}"
+}
+
+resource "aws_cloudwatch_event_target" "stop_instances_event_target" {
+  target_id = "stop_instances_lambda_target"
+  rule = "${aws_cloudwatch_event_rule.stop_instances_event_rule.name}"
+  arn = "${aws_lambda_function.ec2_stop_scheduler_lambda.arn}"
+}
+
+# AWS Lambda Permissions: Allow CloudWatch to execute the Lambda Functions
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_start_scheduler" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.ec2_start_scheduler_lambda.function_name}"
+  principal = "events.amazonaws.com"
+  source_arn = "${aws_cloudwatch_event_rule.start_instances_event_rule.arn}"
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_stop_scheduler" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.ec2_stop_scheduler_lambda.function_name}"
+  principal = "events.amazonaws.com"
+  source_arn = "${aws_cloudwatch_event_rule.stop_instances_event_rule.arn}"
 }
