c3434d7b7f3d9a939ed4d80a947c17812559edc4
Vinod1908 <55542031+Vinod1908@users.noreply.github.com>
2020-04-24 17:15:03 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,11 +1,6 @@
-module "lambda-scheduler" {
-  source = "neillturner/lambda-scheduler/aws"
-  version = "0.x.0"
-  schedule_expression = "cron(5 * * * ? *)"
-  tag = "schedule"
-  schedule_tag_force = "true"
-  ec2_schedule = "true"
-  rds_schedule = "true"
-  default = "{\"mon\": {\"start\": [7], \"stop\": [19]},\"tue\": {\"start\": [7], \"stop\": [19]},\"wed\": {\"start\": [9, 22], \"stop\": [19]},\"thu\": {\"start\": [7], \"stop\": [2,19]}, \"fri\": {\"start\": [18], \"stop\": [9]}, \"sat\": {\"start\": [22]}, \"sun\": {\"stop\": [7]}}"
-  time = "Asia/Kolkata"
+resource "aws_cloudformation_stack" "instance-scheduler" {
+  name = "instance-scheduler"
+
+  template_url = "https://s3.amazonaws.com/solutions-reference/aws-instance-scheduler/latest/instance-scheduler.template"
+
 }
