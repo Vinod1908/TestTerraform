a0ff80f8438cc55cc9406359264047169b2a6651
Vinod1908 <55542031+Vinod1908@users.noreply.github.com>
2020-06-12 14:58:46 +0530

    Update instanceScheduler.tf

--- instanceScheduler.tf
+++ instanceScheduler.tf
@@ -551,6 +551,10 @@ template_body = <<STACK
             "Default":"us-east-1",
             "Description": "List of regions in which instances are scheduled, leave blank for current region only."
         },
+         "CrossAccountRoles": {	
+            "Type": "CommaDelimitedList",	
+            "Description": "Comma separated list of ARN's for cross account access roles. These roles must be created in all checked accounts the scheduler to start and stop instances."	
+        },
         "StartedTags": {
             "Type": "String",
             "Default":"schedStarted=true",
@@ -605,6 +609,7 @@ template_body = <<STACK
                         "TagName",
                         "ScheduledServices",
                         "ScheduleRdsClusters",
+                        "CrossAccountRoles",
                         "CreateRdsSnapshot",
                         "SchedulingActive",
                         "Regions",
@@ -647,6 +652,9 @@ template_body = <<STACK
                 },
                 "SchedulingActive": {
                     "default": "Scheduling enabled"
+                },
+                 "CrossAccountRoles": {	
+                    "default": "Cross-account roles"	
                 },
                 "ScheduleLambdaAccount": {
                     "default": "This account"
@@ -761,7 +769,9 @@ template_body = <<STACK
                             "Sid": "default",
                             "Effect": "Allow",
                             "Principal": {
-                                "AWS": "arn:aws:iam::842856138791:root"
+                                "AWS": {	
+                                    "Fn::Sub": "arn:${AWS::Partition}:iam::${AWS::AccountId}:root"	
+                                }	
                             },
                             "Action": "kms:*",
                             "Resource": "*"
